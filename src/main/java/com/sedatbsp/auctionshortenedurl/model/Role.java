b0165d0c1e65586e0489df1b3ac5bacb0634b68c
Sedat Başpınar <sdtbsp10@gmail.com>
2021-10-02 01:53:46 +0300

    User model created

---
+++ src/main/java/com/sedatbsp/auctionshortenedurl/model/Role.java
@@ -0,0 +1,14 @@
+package com.sedatbsp.auctionshortenedurl.model;
+
+/**
+ * @author Sedat Başpınar
+ * @created 02.10.2021 - 1:51 AM
+ * @project auction-shortened-url
+ */
+public enum Role {
+
+    USER, //default
+    ADMIN, //admin
+    SYSTEM_ADMIN //internal operations
+
+}
---
+++ src/main/java/com/sedatbsp/auctionshortenedurl/model/User.java
@@ -0,0 +1,35 @@
+package com.sedatbsp.auctionshortenedurl.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Sedat Başpınar
+ * @created 02.10.2021 - 1:48 AM
+ * @project auction-shortened-url
+ */
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username",unique = true,nullable = false,length = 100)
+    private String username;
+
+    @Column(name = "password",nullable = false,length = 100)
+    private String password;
+
+    @Column(name = "name",nullable = false,length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "create_time",nullable = false)
+    private LocalDateTime createTime;
+
+}
