716948d92da0dbb7f0f1238f5777f16178f04c6c
Sedat Başpınar <sdtbsp10@gmail.com>
2021-10-05 02:11:24 +0300

    Url repository added.

---
+++ src/main/java/com/sedatbsp/auctionshortenedurl/repository/IUrlRepository.java
@@ -0,0 +1,17 @@
+package com.sedatbsp.auctionshortenedurl.repository;
+
+import com.sedatbsp.auctionshortenedurl.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Sedat Başpınar
+ * @created 05.10.2021 - 2:08 AM
+ * @project auction-shortened-url
+ */
+public interface IUrlRepository extends JpaRepository<Url,Long> {
+
+    Optional<Url> findByShortenedUrl(String shortenedUrl);
+
+}
